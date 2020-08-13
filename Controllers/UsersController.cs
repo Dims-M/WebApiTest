e608dce2d6a0edf4c6a329b1f2b50440a29855db
Дмитрий <rabolan@mail.ru>
2020-08-13 22:43:15 +0300

    ntcns c fgb

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -45,12 +47,38 @@ namespace WebApiTest.Controllers
             
         }
 
+        // Тестовые методы
+      //  [ApiController]
+       // [Route("api/[AddFile]")]
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile != null)
+            {
+                // путь к папке Files
+                string path = "/Files/" + uploadedFile.FileName;
+                // сохраняем файл в папку Files в каталоге wwwroot
+                //using (var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                //{
+                //    await uploadedFile.CopyToAsync(fileStream);
+                //}
+                FileModel file = new FileModel { Name = uploadedFile.FileName, Path = path };
+               // _context.Files.Add(file);
+               // _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
         /// <summary>
         /// Получаем пользователя по id
         /// </summary>
         /// <param name="id">id нужного пользователя.</param>
         /// <returns></returns>
-        // GET api/users/5
+        // GET api/users/5 https://localhost:44325/api/users/1
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> Get(int id)
         {
@@ -68,23 +96,36 @@ namespace WebApiTest.Controllers
         /// <returns>Возрат результата. Если без ошибок то ОК -200</returns>
         // POST api/users
         [HttpPost]
-        public async Task<ActionResult<User>> Post(User user)
+        public async Task<ActionResult<User>> Post(User user) //
+      //  public async Task<ActionResult<User>> Post(IFormFile uploadedFile) //
         {
-            if (user.Age == 99)
-                ModelState.AddModelError("Age", "Возраст не должен быть равен 99");
+           //string temp = uploadedFile.ToString();
+            //if (user.Age == 99)
+            //    ModelState.AddModelError("Age", "Возраст не должен быть равен 99");
 
-            if (user.Name == "admin")
-            {
-                ModelState.AddModelError("Name", "Недопустимое имя пользователя - admin");
-            }
-            // если есть лшибки - возвращаем ошибку 400
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
+            //if (user.Name == "admin")
+            //    if (user.Name == "admin")
+            //{
+            //    ModelState.AddModelError("Name", "Недопустимое имя пользователя - admin");
+            //}
+            //// если есть лшибки - возвращаем ошибку 400
+            //if (!ModelState.IsValid)
+            //    return BadRequest(ModelState);
 
-            db.Users.Add(user);
-            await db.SaveChangesAsync(); // сохранение в Бд
-            return Ok(user);
+            //db.Users.Add(user);
+            //await db.SaveChangesAsync(); // сохранение в Бд
+           // return Ok(user);
+            return Ok();
         }
+
+
+        //[HttpPost]
+        //public ActionResult Index(HttpPostedFileBase upload)
+        //{
+        //    //Вот здесь я хочу принять файл и записать его
+        //    return Content("ура- файл записан");
+
+        //}
 
         /// <summary>
         /// Обновление текущего пользователя.
